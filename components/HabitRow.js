28d91426f93be3769c2e24532b460f55c5212571
Blizzcane <blizzcane@gmail.com>
2022-01-03 22:41:10 -0600

    box color done, need to dynamically alter opacity

--- components/HabitRow.js
+++ components/HabitRow.js
@@ -2,23 +2,56 @@ import styles from "../styles/HabitRow.module.css";
 
 //this component will return a row for the weekly calendar for a habit
 export default function HabitRow({ day, habit }) {
+ 
+  const isComplete = (day) => {
+    if (day) {
+      return completeColor(habit.habit_color);
+    } else {
+      return incompleteColor(habit.habit_color);
+    }
+  };
+
+  const completeColor = (hexCode) => {
+    return {
+      background: `#${hexCode}`,
+      width: "30px",
+      height: "30px",
+      borderRadius: "5px",
+    };
+  };
+
+  const incompleteColor = (hexCode) => {
+    return {
+      background: `#${hexCode}`,
+      width: "30px",
+      height: "30px",
+      borderRadius: "5px",
+    };
+  };
+
   return (
     <tr>
       <th scope="row">{habit.habit_name}</th>
       <td>
-        <div className={styles.box}></div>
+        <div style={isComplete(habit.Sun)}></div>
       </td>
       <td>
-        <div className={styles.box}></div>
+        <div style={isComplete(habit.Mon)}></div>
       </td>
       <td>
-        <div className={styles.box}></div>
+        <div style={isComplete(habit.Tue)}></div>
       </td>
       <td>
-        <div className={styles.box}></div>
+        <div style={isComplete(habit.Wed)}></div>
       </td>
       <td>
-        <div className={styles.box}></div>
+        <div style={isComplete(habit.Thu)}></div>
+      </td>
+      <td>
+        <div style={isComplete(habit.Fri)}></div>
+      </td>
+      <td>
+        <div style={isComplete(habit.Sat)}></div>
       </td>
       <td>total</td>
     </tr>
--- components/Weekday.js
+++ components/Weekday.js
@@ -6,11 +6,13 @@ export default function Weekday({ habits }) {
     <table>
       <tr>
         <td></td>
+        <th scope="col">Sun</th>
         <th scope="col">Mon</th>
         <th scope="col">Tue</th>
         <th scope="col">Wed</th>
         <th scope="col">Thu</th>
         <th scope="col">Fri</th>
+        <th scope="col">Sat</th>
       </tr> 
       {habits.data.map((habit) => {
         return <HabitRow habit={habit} /> ;
