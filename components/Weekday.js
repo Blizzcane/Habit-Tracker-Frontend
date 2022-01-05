3cd1f93f104f29fba61c26e052acdc6025083d9b
Blizzcane <blizzcane@gmail.com>
2022-01-04 21:26:22 -0600

    fixing console errors

--- components/Weekday.js
+++ components/Weekday.js
@@ -4,19 +4,21 @@ import HabitRow from "./HabitRow";
 export default function Weekday({ habits }) {
   return (
     <table>
-      <tr>
-        <td></td>
-        <th scope="col">Sun</th>
-        <th scope="col">Mon</th>
-        <th scope="col">Tue</th>
-        <th scope="col">Wed</th>
-        <th scope="col">Thu</th>
-        <th scope="col">Fri</th>
-        <th scope="col">Sat</th>
-      </tr> 
-      {habits.data.map((habit) => {
-        return <HabitRow habit={habit} /> ;
-      })}
+      <tbody>
+        <tr>
+          <td></td>
+          <th scope="col">Sun</th>
+          <th scope="col">Mon</th>
+          <th scope="col">Tue</th>
+          <th scope="col">Wed</th>
+          <th scope="col">Thu</th>
+          <th scope="col">Fri</th>
+          <th scope="col">Sat</th>
+        </tr>
+        {habits.data.map((habit) => {
+          return <HabitRow key={habit.habit_id} habit={habit} />;
+        })}
+      </tbody>
     </table>
   );
 }
